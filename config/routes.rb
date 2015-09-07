e2d37d49ceee1ee7f5e456fee4d05dfcf46d93e5
Jesse Szwedko <jesse.szwedko@gmail.com>
2015-09-07 10:14:35 -0700

    Add Categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :categories, except: [:new, :edit]
+  resources :categories, except: [:new, :edit]
   resources :phone_numbers, except: [:new, :edit]
   resources :resources, except: [:new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
