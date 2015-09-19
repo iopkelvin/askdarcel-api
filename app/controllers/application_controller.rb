e0c6a8040dd31725f5b586e2b8b114580077f128
Jesse Szwedko <jesse.szwedko@gmail.com>
2015-09-19 15:44:45 -0700

    Catch routing and activerecordnotfound errors
    And return JSON 404 response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::API
   include ActionController::Serialization
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActionController::RoutingError, with: :not_found
+
+  def not_found
+    render json: { message: "Not found" }
+  end
+
+  def routing_error
+    raise ActionController::RoutingError.new(params[:path])
+  end
 end
