7202356c68a79cb21f0ccc34775218d717da5e38
Jesse Szwedko <jesse.szwedko@gmail.com>
2015-09-20 14:42:14 -0700

    Make paperclip bucket configurable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = { storage: :s3, bucket: 'sheltertech-images' }
+  config.paperclip_defaults = { storage: :s3, bucket: ENV['PAPERCLIP_S3_BUCKET'] }
 end
