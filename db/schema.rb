2efc973355cf96e898229c653bb3965697260457
James Hamlin <jfhamlin@gmail.com>
2016-03-20 19:11:06 -0700

    [migration] Add unique index to categories on name.
    Signed-off-by: James Hamlin <jfhamlin@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320200142) do
+ActiveRecord::Schema.define(version: 20160321021009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20160320200142) do
     t.datetime "updated_at", null: false
     t.string   "name",       null: false
   end
+
+  add_index "categories", ["name"], name: "index_categories_on_name", unique: true, using: :btree
 
   create_table "categories_resources", id: false, force: :cascade do |t|
     t.integer "category_id", null: false
