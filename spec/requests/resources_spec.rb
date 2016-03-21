b41600aa8f49352e1279eb80a19d0e900de5e01c
James Hamlin <jfhamlin@gmail.com>
2016-03-20 17:53:41 -0700

    Add resource, category factories and resources request specs.
    - Adds factory_girl_rails gem.
    - Adds two simple request specs to test the basic resource API.
    
    Signed-off-by: James Hamlin <jfhamlin@gmail.com>

---
+++ spec/requests/resources_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe 'Resources' do
+  context 'index' do
+    context 'without a category_id' do
+      let!(:resources) { create_list :resource, 4 }
+
+      it 'returns all resources' do
+        get '/resources'
+        returned_ids = response_json.map { |r| r['id'] }
+        expect(returned_ids).to match(resources.map(&:id))
+      end
+    end
+
+    context 'with a category_id' do
+      let!(:category_a) { create :category, name: 'a' }
+      let!(:category_b) { create :category, name: 'b' }
+      let!(:resources) do
+        create_list :resource, 2, categories: []
+      end
+      let!(:resources_a) do
+        create_list :resource, 2, categories: [category_a]
+      end
+      let!(:resources_b) do
+        create_list :resource, 2, categories: [category_b]
+      end
+
+      it 'returns only resources with that category' do
+        get "/resources?category_id=#{category_a.id}"
+        returned_ids = response_json.map { |r| r['id'] }
+        expect(returned_ids).to match(resources_a.map(&:id))
+      end
+    end
+  end
+end
