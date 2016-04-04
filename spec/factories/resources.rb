809f4d075bd3bfd6cedbfe6d30c7049f25438739
Joe Freund <jfreund@myvest.com>
2016-04-04 13:22:14 -0700

    Placated teh cop

--- spec/factories/resources.rb
+++ spec/factories/resources.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :resource do
     name { Faker::Company.name }
-    after (:create) do |resource|
+    after :create do |resource|
       create(:address, resource: resource)
       create(:phone, resource: resource)
       create(:schedule, resource: resource)
--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -3,13 +3,10 @@ FactoryGirl.define do
     resource nil
 
     after(:create) do |schedule|
-      array = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"]
-     
-      array.each { |day|
-        create(:schedule_day, day: day, schedule: schedule)  
-      }
-
+      array = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+      array.each do |day|
+        create(:schedule_day, day: day, schedule: schedule)
+      end
     end
-    
   end
 end
