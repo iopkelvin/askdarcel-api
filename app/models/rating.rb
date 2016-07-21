f393cbc104b52b3c5c638f85e75f5e7ade03b48c
James Hamlin <jfhamlin@gmail.com>
2016-07-20 23:33:47 -0700

    Update Rating model, removing association, adding validation.
    Signed-off-by: James Hamlin <jfhamlin@gmail.com>

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,10 @@
 class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :resource
-  belongs_to :service
   has_one :review
+
+  validates :rating, numericality: {
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5
+  }
 end
