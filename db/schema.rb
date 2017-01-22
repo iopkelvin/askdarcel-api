7bc632070a563fd486b20ff694b7a5380db8fcf7
jjfreund <jjfreund@gmail.com>
2017-01-22 13:13:27 -0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 
 ActiveRecord::Schema.define(version: 20170121235411) do
-ActiveRecord::Schema.define(version: 20170112020108) do
 
 
   # These are extensions that must be enabled in order to support this database
