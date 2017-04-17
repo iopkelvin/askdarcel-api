7e534cc547a557fe1190c1e7b32d496f3d1ef7de
Leland Garofalo <lelandg@gmail.com>
2017-04-16 21:31:22 -0700

    Adjust cacheable to 5 minutes and add to Categories and Resources (#164)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
   end
 
   def cachable
-    expires_in 20.minute, public: true, must_revalidate: true
+    expires_in 5.minute, public: true, must_revalidate: true
   end
 
   private
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :cachable
+
   def index
     render json: CategoryPresenter.present(Category.all)
   end
