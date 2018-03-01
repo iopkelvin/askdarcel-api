a1171a6cc7740e9e758c47e25132822fd8dfd0d1
Joe Freund <jfreund@myvest.com>
2018-02-28 20:53:26 -0800

    Triaging resource listing performance issues

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -4,12 +4,10 @@ class ResourcesController < ApplicationController
   def index
     category_id = params.require :category_id
     # TODO: This can be simplified once we remove categories from resources
-    relation =
-      resources
-      .joins(:address)
-      .where(categories_join_string, category_id, category_id)
-      .where(status: Resource.statuses[:approved])
-      .order(sort_order)
+    relation = resources.joins(:categories).joins(:address)
+                        .where('categories.id' => category_id).where(status: Resource.statuses[:approved])
+                        .order(sort_order)
+
     render json: ResourcesPresenter.present(relation)
   end
 
