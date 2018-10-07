14ddfc7e9242daf2a219b483602af0467ac21133
Leland Garofalo <lgarofalo@users.noreply.github.com>
2018-10-06 21:30:34 -0700

    fix(logging): log to stdout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,5 +75,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+  config.logger = Logger.new(STDOUT)
 end
