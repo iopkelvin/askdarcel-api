1053c89cd949055859dbfe415f6b230d033991d7
Leland Garofalo <lgarofalo@users.noreply.github.com>
2018-10-06 23:09:04 -0700

    fix(puma.rb): make rubocop hush

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
-#
-stdout_redirect(stdout = '/dev/stdout', stderr = '/dev/stderr', append = true)
+
+stdout_redirect(stdout = '/dev/stdout', stderr = '/dev/stderr', append = true) # rubocop:disable UselessAssignment
