5e24e2436082578d520c68fe1813a5a81285fdff
Max Stuart <maxastuart@gmail.com>
2019-11-20 17:16:17 -0800

    Update algolia_reindex_job.rb

--- app/jobs/algolia_reindex_job.rb
+++ app/jobs/algolia_reindex_job.rb
@@ -15,14 +15,15 @@
 class AlgoliaReindexJob
   def perform
     Resource.where(status: :approved).reindex
-    # Since Service and Resource share an algolia index, we use `reindex!` so
-    # Service reindexing does not clear out Resource index records.
-    Service.where(status: :approved).reindex!
 
     # Batch synonyms, with replica forwarding and atomic replacement of existing synonyms
     forward_to_replicas = true
     replace_existing_synonyms = true
     Resource.index.batch_synonyms(format_synonyms_list, forward_to_replicas, replace_existing_synonyms)
+
+    # Since Service and Resource share an algolia index, we use `reindex!` so
+    # Service reindexing does not clear out Resource index records.
+    Service.where(status: :approved).reindex!
   end
 
   private
