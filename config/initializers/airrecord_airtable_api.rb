6c26dc5a5992ebd0fcbd560b3d022ce71fe5af9d
Walter <walter@Walters-MacBook-Pro.local>
2020-03-11 21:02:26 -0700

    Add env variables

--- config/initializers/airrecord_airtable_api.rb
+++ config/initializers/airrecord_airtable_api.rb
@@ -4,7 +4,7 @@ if Rails.configuration.x.airtable.api_key
   Airrecord.api_key = Rails.configuration.x.airtable.api_key
 
   class AirTableOrgs < Airrecord::Table
-    self.base_key = "appItSYNzRjTx08hN"
-    self.table_name = "AskDarcel Organizations"
+    self.base_key = ENV['AIRTABLE_BASE_KEY']
+    self.table_name = ENV['AIRTABLE_TABLE_NAME']
   end
 end
