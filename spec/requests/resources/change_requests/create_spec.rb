1581d58c164bac87cf8c4d7896bd2e77ed64b6dd
Richard Xia <richardxia@richardxia.com>
2020-05-17 20:48:54 -0700

    Fix rspec tests.

--- spec/requests/resources/change_requests/create_spec.rb
+++ spec/requests/resources/change_requests/create_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'Resource Change Requests' do
   it 'creates a change request and associated field changes' do
     resource.update(updated_at: 1.hour.ago)
 
-    post "/resources/#{resource.id}/change_requests", params: { change_request: params }
+    post "/resources/#{resource.id}/change_requests", params: { change_request: { action: '1', field_changes: params } }
 
     expect(resource.reload.name).to eq(params[:name])
     expect(resource.long_description).to eq(params[:long_description])
@@ -41,6 +41,7 @@ RSpec.describe 'Resource Change Requests' do
         'status' => 'pending',
         'type' => 'ResourceChangeRequest',
         'object_id' => change_request.object_id,
+        'action' => 'edit',
         'field_changes' => [
           { 'field_name' => 'name', 'field_value' => params[:name] },
           { 'field_name' => 'long_description', 'field_value' => params[:long_description] }
@@ -55,7 +56,7 @@ RSpec.describe 'Resource Change Requests' do
     end
 
     it 'rolls back changes and returns bad request status with error message' do
-      post "/resources/#{resource.id}/change_requests", params: { change_request: params }
+      post "/resources/#{resource.id}/change_requests", params: { change_request: { action: '1', field_changes: params } }
 
       expect(resource.change_requests).to be_empty
       expect(FieldChange.all).to be_empty
