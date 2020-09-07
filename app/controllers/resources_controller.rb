9e3bd3a9a7333da9db2b917a3e2908e3e9e487b9
mac <mac@macs-mbp.lan>
2020-09-06 21:17:12 -0700

    fix travis-ci offenses

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -155,7 +155,7 @@ class ResourcesController < ApplicationController
     # DB by several orders of magnitude due to duplication of tuples.
     Resource.preload(:addresses, :phones, :categories, :notes,
                      schedule: :schedule_days,
-                     services: [:notes, :categories, { schedule: :schedule_days }, :eligibilities],)
+                     services: [:notes, :categories, { schedule: :schedule_days }, :eligibilities])
   end
 
   def sort_order
