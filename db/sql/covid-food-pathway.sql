9b4c405ba291fdf966ecd3e02e0321580dcdcdd2
Joe Freund <jfreund@myvest.com>
2020-10-04 16:19:26 -0700

    Added subcategories for covid-food

---
+++ db/sql/covid-food-pathway.sql
@@ -0,0 +1,295 @@
+insert into categories (id, created_at, updated_at, name, top_level, 
+vocabulary, featured) values 
+(1100035, '14-JUN-20', '14-JUN-20', 'Food pantries', 'f', null, 'f');
+
+insert into categories (id, created_at, updated_at, name, top_level, 
+vocabulary, featured) values 
+(1100036, '14-JUN-20', '14-JUN-20', 'Daily free meals for all', 'f', null, 'f');
+
+insert into categories (id, created_at, updated_at, name, top_level, 
+vocabulary, featured) values 
+(1100037, '14-JUN-20', '14-JUN-20', 'Food for children', 'f', null, 'f');
+
+insert into categories (id, created_at, updated_at, name, top_level, 
+vocabulary, featured) values 
+(1100038, '14-JUN-20', '14-JUN-20', 'Home-delivered groceries and home-delivered meals', 'f', null, 'f');
+
+insert into categories (id, created_at, updated_at, name, top_level, 
+vocabulary, featured) values 
+(1100039, '14-JUN-20', '14-JUN-20', 'Congregate meals for seniors and people with disabilities', 'f', null, 'f');
+
+insert into categories (id, created_at, updated_at, name, top_level, 
+vocabulary, featured) values 
+(1100040, '14-JUN-20', '14-JUN-20', 'Food benefits', 'f', null, 'f');
+
+insert into category_relationships (parent_id, child_id) values 
+(1000001,1100035);
+
+insert into category_relationships (parent_id, child_id) values 
+(1000001,1100036);
+
+insert into category_relationships (parent_id, child_id) values 
+(1000001,1100037);
+
+insert into category_relationships (parent_id, child_id) values 
+(1000001,1100038);
+
+insert into category_relationships (parent_id, child_id) values 
+(1000001,1100039);
+
+insert into category_relationships (parent_id, child_id) values 
+(1000001,1100040);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,1871);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,709);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,2878);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,154);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,90);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,226);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,771);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,96);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,181);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,68);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,1546);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,3);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,82);
+
+insert into categories_services (category_id, service_id) values 
+(1100035,1365);
+
+insert into categories_services (category_id, service_id) values 
+(1100036,305);
+
+insert into categories_services (category_id, service_id) values 
+(1100036,441);
+
+insert into categories_services (category_id, service_id) values 
+(1100036,86);
+
+insert into categories_services (category_id, service_id) values 
+(1100036,141);
+
+insert into categories_services (category_id, service_id) values 
+(1100036,73);
+
+insert into categories_services (category_id, service_id) values 
+(1100036,2244);
+
+insert into categories_services (category_id, service_id) values 
+(1100036,392);
+
+insert into categories_services (category_id, service_id) values 
+(1100036,14);
+
+insert into categories_services (category_id, service_id) values 
+(1100037,2759);
+
+insert into categories_services (category_id, service_id) values 
+(1100037,1870);
+
+insert into categories_services (category_id, service_id) values 
+(1100038,1549);
+
+insert into categories_services (category_id, service_id) values 
+(1100038,2062);
+
+insert into categories_services (category_id, service_id) values 
+(1100038,1179);
+
+insert into categories_services (category_id, service_id) values 
+(1100038,2877);
+
+insert into categories_services (category_id, service_id) values 
+(1100039,2783);
+
+insert into categories_services (category_id, service_id) values 
+(1100039,125);
+
+insert into categories_services (category_id, service_id) values 
+(1100039,2768);
+
+insert into categories_services (category_id, service_id) values 
+(1100039,82);
+
+insert into categories_services (category_id, service_id) values 
+(1100039,2061);
+
+insert into categories_services (category_id, service_id) values 
+(1100039,158);
+
+insert into categories_services (category_id, service_id) values 
+(1100040,1455);
+
+insert into categories_services (category_id, service_id) values 
+(1100040,1451);
+
+insert into categories_services (category_id, service_id) values 
+(1100040,1454);
+
+insert into categories_services (category_id, service_id) values 
+(1100040,1452);
+
+insert into categories_services (category_id, service_id) values 
+(1100040,2825);
+
+insert into categories_services (category_id, service_id) values 
+(1100040,1453);
+
+insert into categories_services (category_id, service_id) values 
+(1100040,2461);
+
+insert into categories_services (category_id, service_id) values 
+(1100040,1858);
+
+
+
+insert into categories_services (category_id, service_id) values 
+(1000001,1871);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,709);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,2878);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,154);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,90);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,226);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,771);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,96);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,181);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,68);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,1546);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,3);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,82);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,1365);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,305);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,441);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,86);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,141);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,73);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,2244);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,392);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,14);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,2759);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,1870);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,1549);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,2062);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,1179);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,2877);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,2783);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,125);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,2768);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,82);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,2061);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,158);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,1455);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,1451);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,1454);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,1452);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,2825);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,1453);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,2461);
+
+insert into categories_services (category_id, service_id) values 
+(1000001,1858);
