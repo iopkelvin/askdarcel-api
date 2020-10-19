61d77a5030aa7fe00d033f1d8ea174aa84b0ccb2
meltingmelon <me@bigbucksmel.com>
2020-10-18 17:12:05 -0700

    fixed syntax errors and incorrect category ids. Ran in staging

--- db/sql/covid-shelter.sql
+++ db/sql/covid-shelter.sql
@@ -1,29 +1,29 @@
 -- Covid Shelter Category
 insert into categories (id, created_at, updated_at, name, top_level, vocabulary, featured) values 
-(1000010, '04-OCT-20', '04-OCT-20', 'Covid-shelter', 'f', null, 'f');
+(1000010, '04-OCT-20', '04-OCT-20', 'Covid-shelter', 't', null, 'f');
 
 -- Covid Shelter Subcategories
 insert into categories (id, created_at, updated_at, name, top_level, vocabulary, featured) values
-(1100034, '18-OCT-20', 'We are a family and we need shelter', 'f', null, 'f');
+(1100041, '18-OCT-20', '18-OCT-20', 'We are a family and we need shelter', 'f', null, 'f');
 
 insert into categories (id, created_at, updated_at, name, top_level, vocabulary, featured) values
-(1100035, '18-OCT-20', 'I am someone between 18-24 year old in need of shelter', 'f', null, 'f');
+(1100042, '18-OCT-20', '18-OCT-20', 'I am someone between 18-24 year old in need of shelter', 'f', null, 'f');
 
 insert into categories (id, created_at, updated_at, name, top_level, vocabulary, featured) values
-(1100036, '18-OCT-20', 'I am a single adult and I need shelter', 'f', null, 'f');
+(1100043, '18-OCT-20',  '18-OCT-20', 'I am a single adult and I need shelter', 'f', null, 'f');
 
 -- insert subcategories in Covid-shelter
 
 insert into category_relationships (parent_id, child_id) values
-(1000010, 1100034);
+(1000010, 1100041);
 
 insert into category_relationships (parent_id, child_id) values
-(1000010, 1100035);
+(1000010, 1100042);
 
 insert into category_relationships (parent_id, child_id) values
-(1000010, 1100036);
+(1000010, 1100043);
 
--- Inset services to Covid-shelter category
+-- Insert services to Covid-shelter category
 
 insert into categories_services (category_id, service_id) values (1000010, 2559);
 
@@ -49,32 +49,24 @@ insert into categories_services (category_id, service_id) values (1000010, 2670)
 
 --insert services to subcategories
 
-insert into categories_services (category_id, service_id) values (1100034, 2559);
+insert into categories_services (category_id, service_id) values (1100041, 2559);
 
-insert into categories_services (category_id, service_id) values(1100034, 2560);
+insert into categories_services (category_id, service_id) values(1100041, 2560);
 
-insert into categories_services (category_id, service_id) values(1100034, 228);
+insert into categories_services (category_id, service_id) values(1100041, 228);
 
-insert into categories_services (category_id, service_id) values(1100034, 49);
+insert into categories_services (category_id, service_id) values(1100041, 49);
 
-insert into categories_services (category_id, service_id) values (1100034, 2872);
+insert into categories_services (category_id, service_id) values (1100041, 2872);
 
-insert into categories_services (category_id, service_id) values (1100035, 2032);
+insert into categories_services (category_id, service_id) values (1100042, 2032);
 
-insert into categories_services (category_id, service_id) values (1100035, 2030);
+insert into categories_services (category_id, service_id) values (1100042, 2030);
 
-insert into categories_services (category_id, service_id) values (1100035, 719);
+insert into categories_services (category_id, service_id) values (1100042, 719);
 
-insert into categories_services (category_id, service_id) values (1100035, 2781);
+insert into categories_services (category_id, service_id) values (1100042, 2781);
 
-insert into categories_services (category_id, service_id) values (1100036, 306);
+insert into categories_services (category_id, service_id) values (1100043, 306);
 
-insert into categories_services (category_id, service_id) values (1100036, 2670);
-
---  Saw this in pathways and not sure if applicable for covid-shelter's eligibility
--- update eligibilities set name = 'We are a family and we need shelter' where name = 'Families';
--- update eligibilities set name = 'I am someone between 18-24 years old in need of shelter' where name = 'Youth';
--- update eligibilities set name = 'I am a single adult and I need shelter' where name = 'Single Adult';
-
-
-
+insert into categories_services (category_id, service_id) values (1100043, 2670);
