6bae8ad9d7167d8ffd4f073dff776c2738263bab
Katerina Kossler <katerinakossler@gmail.com>
2020-10-20 21:23:49 -0700

    Trying this one more time

--- app/models/site.rb
+++ app/models/site.rb
@@ -4,7 +4,6 @@ class Site < ActiveRecord::Base
   has_and_belongs_to_many(:categories,
                           join_table: "categories_sites",
                           foregin_key: "site_id")
-  
   has_and_belongs_to_many(:resources,
                           join_table: "resources_sites",
                           foregin_key: "site_id")
