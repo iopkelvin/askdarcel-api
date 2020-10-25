66d6f91934108481c3eeff7b0b5825051fde574d
Akhtam <akhtam.ismatov@gmail.com>
2020-10-25 01:41:01 -0700

    fix failed checks

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,7 +2,7 @@
 
 class FeedbacksController < ApplicationController
   before_action :load_source
-  
+
   def index
     feedbacks = @feedback_source.feedbacks
     render json: FeedbackPresenter.present(feedbacks)
@@ -12,17 +12,17 @@ class FeedbacksController < ApplicationController
     feedback_params = { feedback: {
       rating: params[:rating],
       review_attributes: { review: params[:review], tags: params[:tags] }
-    }}
+    } }
 
     if @feedback_source.feedbacks.create(feedback_params[:feedback])
       render status: :created, json: 'Success!'
     else
       render json: feedback.errors.full_messages, status: :unprocessable_entity
     end
-
   end
 
   private
+
   def load_source
     source_type, id = request.path.split('/')[1, 2]
     @feedback_source = source_type.classify.constantize.find(id)
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Feedback < ApplicationRecord
-  validates :rating, inclusion: { in: [ true, false ] }
+  validates :rating, inclusion: { in: [true, false] }
   belongs_to :resource
   belongs_to :service
   has_one :review, dependent: :destroy
-  accepts_nested_attributes_for :review, reject_if: proc {|attributes| attributes['review'].blank? && attributes['tags'].blank?}
+  accepts_nested_attributes_for :review, reject_if: proc { |attributes| attributes['review'].blank? && attributes['tags'].blank? }
 end
