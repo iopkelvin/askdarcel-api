decb8de410d66547b9e0c78bf248964772a28f92
Katerina Kossler <katerinakossler@gmail.com>
2020-11-21 22:16:22 -0800

    Add paren in fix_resources for robocop

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -79,7 +79,7 @@ class ResourcesController < ApplicationController
       r.addresses.each do |a|
         fix_lat_and_long(a)
       end
-      r.sites = ['sfsg'] if r.key? :sites && r.sites.empty?
+      r.sites = ['sfsg'] if r.key? (:sites) && r.sites.empty?
     end
   end
 
